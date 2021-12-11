72943737eea5b4dc872313516bc36be2166b42c2
Andypch923 <andypoon923@gmail.com>
2021-12-11 13:36:20 +0800

    DONE

--- src/business.py
+++ src/business.py
@@ -1,30 +1,70 @@
 import socket
 import scrapper
 import DataBase
+from threading import Thread    
+from socketserver import BaseRequestHandler, ThreadingMixIn
+import json
+
+class ClientThread(Thread):
+    
+    def __init__(self,ip,port):
+        Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        print ("[+] New thread started for "+ip+":"+str(port))
+
+
+    def run(self):
+        while True:
+            #send list of countries
+            countryList = database.fetchCountry()
+            y = dict()
+            for i in countryList:
+                y[i] = ""
+            y = json.dumps(y)
+            conn.sendall(bytes(y,encoding ="utf-8"))
+            #receive list of Parameters
+            data = conn.recv(1024)
+            data = data.decode('utf-8')
+            listOfParams = list((json.loads(data)).keys())
+
+            if (int(listOfParams[1]) == 1990 and int(listOfParams[2]) == 2017):
+                dataToSend = database.fetchCountry(listOfParams[0])
+            else:
+                dataToSend = database.fetchDataByCountryAndYear(listOfParams[0],int(listOfParams[1]),int(listOfParams[2]))
+            #send list of data fetched
+            y2 = dict()
+            for i in dataToSend:
+                y2[i] = ""
+            y2 = json.dumps(y2)
+            y2 = y2.encode("utf-8")
+            conn.sendall(y2)  # echo
 
 dataStream= scrapper.scrapper()
 dataStream.groupLists()
 
 database = DataBase.DataBase(dataStream.getListOfRecords())
 
-s = socket.socket()
-port = 12345
-s.bind(('',port))
+TCP_IP = '127.0.0.1'
+TCP_PORT = 12345
+BUFFER_SIZE = 1024  # Normally 1024
 
-s.listen(5)
+
+tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+tcpsock.bind((TCP_IP, TCP_PORT))
+threads = []
 
 while True:
-    
-    c , addr = s.accept()
-    dataFromClient = c.recv(1024)
-    listOfParams = dataFromClient.decode()
-    dataToSend = list()
-    if (listOfParams[1] == 1990 and listOfParams[2] == 2017):
-        dataToSend = database.fetchCountry(listOfParams[0])
-    else:
-        dataToSend = database.fetchDataByCountryAndYear(listOfParams[0],listOfParams[1],listOfParams[2])
-
-    s.send(dataToSend.encode())
-    c.close()
-
+    tcpsock.listen(4)
+    print ("Waiting for incoming connections...")
+    (conn, (ip,port)) = tcpsock.accept()
+    newthread = ClientThread(ip,port)
+    newthread.start()
+    threads.append(newthread)
+    tcpsock.close()
     break
+
+for t in threads:
+    t.join()
+
--- src/user.py
+++ src/user.py
@@ -5,7 +5,7 @@ from tkinter.constants import CENTER, LEFT, TOP
 import tkinter as tk
 from tkinter import *
 import plot
-
+import json
 
 def onselect1(event1):
     w = event1.widget
@@ -28,14 +28,26 @@ def onselect3(event3):
 def sendAndPlot():
     del dataList[2]
     del dataList[3]
-    for i in dataList:
-        s.send(i.encode())
-    dataFromServer = list(s.recv(1024))
-    grapher = plot.Graph()
-    rangedYearList = [*range(dataList[1],dataList[2],1)]
-    grapher.xyPlot(rangedYearList,dataFromServer)
-    
+    if int(dataList[1]) > int(dataList[2]) and len(dataList) > 3:
+        print("Error: Ending Year cannot be less than Starting Year")
+        dataList.clear()
+    else:
+        tempDict = dict()
+        for i in dataList:
+            tempDict[i] = ""
+        tempDict = json.dumps(tempDict)
+        s.sendall(bytes(tempDict,encoding ="utf-8"))
 
+        dataFromServer = s.recv(1024)
+        dataFromServer = dataFromServer.decode('utf-8')
+        dataFromServer = list((json.loads(dataFromServer)).keys())
+        floatDataFromServer = []
+        for i in dataFromServer:
+            floatDataFromServer.append(float(i))
+        grapher = plot.Graph()
+        rangedYearList = [*range(dataList[1],dataList[2],1)]
+        grapher.xyPlot(rangedYearList,floatDataFromServer)
+        dataList.clear()
 
 dataList = []
 dataStream= scrapper.scrapper()
@@ -46,17 +58,24 @@ database = DataBase.DataBase(dataStream.getListOfRecords())
 s = socket.socket()
 port = 12345
 s.connect(('127.0.0.1', port))
+#Receive List of Countries
+data = s.recv(1024)
+data = data.decode('utf-8')
+countryList = (json.loads(data)).keys()
 
 
 root = tk.Tk()
+root.title('Year and Country: Find your data!')
 root.geometry("900x720")
+
+
 frame = tk.Frame(root, bg = "white")
 
 l1 = Listbox(frame)
 l2 = Listbox(frame)
 l3 = Listbox(frame)
 
-for item in database.fetchCountry():
+for item in countryList:
     l1.insert(END, item)
 
 yearList = [*range(1990,2018,1)]
@@ -70,6 +89,13 @@ l2.grid(row=0, column=1)
 listbox2Select = l2.bind('<<ListboxSelect>>',lambda event2: onselect2(event2))
 l3.grid(row=0, column=2)
 listbox3Select = l3.bind('<<ListboxSelect>>',lambda event3: onselect3(event3))
+
+
+canvas1 = tk.Canvas(root, height =150, width = 700)
+canvas1.create_text(250,100, text="""\n\nClick on the listbox from left to right to select the country, 
+starting year and ending year. This will plot an xy plot of the 
+data from the respectively country.""", fill ="black", font = ('Helvetica 10 italic'))
+canvas1.pack(side=BOTTOM,padx=10,pady=10)
 
 frame.place(relwidth=0.8, relheight =0.8,relx = 0.1, rely=0.1)
 
